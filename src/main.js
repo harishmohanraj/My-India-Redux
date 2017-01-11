4375d0144a02c4d145e61bf6daed3b307742a20f
Harish Mohan Raj <hraj@sapient.com>
2017-01-11 11:16:30 +0000

    injectTagEventAdded

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import AppContainer from './containers/AppContainer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 // ========================================================
 // Store Instantiation
